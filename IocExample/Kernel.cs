ffcb759a29204bc58faf1ca7d1018b2f40b0887e
Maryoku <maryoku.amaya@gmail.com>
2018-05-25 00:53:10 +0500

    added solution with dictionaries and recursion

---
+++ IocExample/Kernel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IocExample
+{
+    class Kernel
+    {
+        private Dictionary<Type, Type> typeDictionary = new Dictionary<Type, Type>();
+        private Dictionary<Type, Object> objectDictionary = new Dictionary<Type, Object>();
+
+        public void BindTypeToType(Type key, Type value)
+        {
+            typeDictionary.Add(key, value);
+        }
+
+        public void BindObjectToType(Type key, Object value)
+        {
+            objectDictionary.Add(key, value);
+        }
+
+        public T GetObject<T>()
+        {
+            return (T)GetObjectOfType(typeof(T));
+        }
+
+        private Object GetObjectOfType(Type objectType)
+        {
+            if (objectDictionary.ContainsKey(objectType))
+            {
+                return objectDictionary[objectType];
+            }
+            else
+            {
+                objectType = typeDictionary[objectType];
+                var constructorInfo = Utils.GetSingleConstructor(objectType);
+                List<Object> parameters = new List<Object>();
+
+                foreach (var parameter in constructorInfo.GetParameters())
+                {
+                    parameters.Add(GetObjectOfType(parameter.ParameterType));
+                }
+
+                return Utils.CreateInstance(objectType, parameters);
+            }
+        }
+    }
+}
---
+++ IocExample/Program.cs
@@ -0,0 +1,61 @@
+﻿using IocExample.Classes;
+
+namespace IocExample
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var logger = new ConsoleLogger();
+        //    var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+        //    var createUserHandler = new CreateUserHandler(new UserService(new QueryExecutor(sqlConnectionFactory), new CommandExecutor(sqlConnectionFactory), new CacheService(logger, new RestClient("API KEY"))), logger);
+
+        //    createUserHandler.Handle();
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    IKernel kernel = new StandardKernel();
+
+        //    kernel.Bind<ILogger>().To<ConsoleLogger>();
+        //    kernel.Bind<UserService>().To<UserService>();
+        //    kernel.Bind<QueryExecutor>().To<QueryExecutor>();
+        //    kernel.Bind<CommandExecutor>().To<CommandExecutor>();
+        //    kernel.Bind<CacheService>().To<CacheService>();
+
+        //    kernel.Bind<RestClient>()
+        //        .ToConstructor(k => new RestClient("API_KEY"));
+
+        //    kernel.Bind<IConnectionFactory>()
+        //        .ToConstructor(k => new SqlConnectionFactory("SQL Connection", k.Inject<ILogger>()))
+        //        .InSingletonScope();
+
+        //    kernel.Bind<CreateUserHandler>().To<CreateUserHandler>();
+
+        //    var createUserHandler = kernel.Get<CreateUserHandler>();
+
+        //    createUserHandler.Handle();
+        //}
+
+        static void Main(string[] args)
+        {
+            Kernel kernel = new Kernel();
+
+            kernel.BindTypeToType(typeof(ILogger), typeof(ConsoleLogger));
+            kernel.BindObjectToType(typeof(RestClient), new RestClient("API_KEY"));
+            kernel.BindTypeToType(typeof(CacheService), typeof(CacheService));
+
+            kernel.BindObjectToType(typeof(IConnectionFactory), new SqlConnectionFactory("SQL Connection", kernel.GetObject<ILogger>()));
+
+            kernel.BindTypeToType(typeof(CommandExecutor), typeof(CommandExecutor));
+            kernel.BindTypeToType(typeof(QueryExecutor), typeof(QueryExecutor));
+            kernel.BindTypeToType(typeof(UserService), typeof(UserService));
+
+            kernel.BindTypeToType(typeof(CreateUserHandler), typeof(CreateUserHandler));
+
+            var createUserHandler = kernel.GetObject<CreateUserHandler>();
+
+            createUserHandler.Handle();
+        }
+    }
+}
